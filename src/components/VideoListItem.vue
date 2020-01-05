113d0ba50e523bd05099b51f7faf605259049df6
Sanath <sanathrpai99@gmail.com>
2020-01-05 19:53:12 +0530

    Used thumbnails

--- src/components/VideoListItem.vue
+++ src/components/VideoListItem.vue
@@ -1,11 +1,20 @@
 <template>
-  <li class="list-group-item">{{video.snippet.title}}</li>
+  <li class="list-group-item">
+    <img :src="thumbnailUrl" />
+    {{video.snippet.title}}
+  </li>
 </template>
 
 <script>
 export default {
   name: "VideoListItem",
-  props: ["video"]
+  props: ["video"],
+  computed: {
+    thumbnailUrl: function() {
+      return this.video.snippet.thumbnails.default.url;
+      //we put this whenever we use prop
+    }
+  }
 };
 </script>
 
